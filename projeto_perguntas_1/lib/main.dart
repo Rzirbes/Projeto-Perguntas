d87abcb0a3ecf5ff7235f7df057d161bb60b1106
Rômulo Zirbes <rzirbes5@rede.ulbra.br>
2022-03-25 11:25:45 -0300

    Retorno de respostas

--- projeto_perguntas_1/lib/main.dart
+++ projeto_perguntas_1/lib/main.dart
@@ -15,7 +15,7 @@ class _PerguntasAppState extends State<PerguntaApp> {
         {'texto': 'Preto', 'pontuacao': 10},
         {'texto': 'Vermelho', 'pontuacao': 5},
         {'texto': 'Branco', 'pontuacao': 3},
-        {'texto': 'Verde', 'pontuacao': 7},
+        {'texto': 'Verde', 'pontuacao': 1},
       ]
     },
     {
@@ -24,7 +24,7 @@ class _PerguntasAppState extends State<PerguntaApp> {
         {'texto': 'Coelho', 'pontuacao': 10},
         {'texto': 'Cobra', 'pontuacao': 5},
         {'texto': 'Elefante', 'pontuacao': 3},
-        {'texto': 'Leão', 'pontuacao': 7},
+        {'texto': 'Leão', 'pontuacao': 1},
       ]
     },
     {
@@ -33,7 +33,7 @@ class _PerguntasAppState extends State<PerguntaApp> {
         {'texto': 'Maria', 'pontuacao': 10},
         {'texto': 'João', 'pontuacao': 5},
         {'texto': 'Leo', 'pontuacao': 3},
-        {'texto': 'Pedro', 'pontuacao': 7},
+        {'texto': 'Pedro', 'pontuacao': 1},
       ]
     }
   ];
